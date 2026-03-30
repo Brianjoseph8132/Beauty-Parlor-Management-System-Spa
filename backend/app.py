35c67156e2812bca1d291fe2ccb1d7c5a4ddb217
Brian Joseph <brianjoseph8132@gmail.com>
2026-03-30 06:36:42 +0300

    changes the email details

--- backend/app.py
+++ backend/app.py
@@ -45,9 +45,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'ashley.testingmoringa@gmail.com'  
-app.config['MAIL_PASSWORD'] ='wksb hzbp lyqu wyxo'  
-app.config['MAIL_DEFAULT_SENDER'] = "ashley.testingmoringa@gmail.com"
+app.config['MAIL_USERNAME'] = 'brianadrian81@gmail.com'  
+app.config['MAIL_PASSWORD'] ='rryp vdqd ibnw ayma'  
+app.config['MAIL_DEFAULT_SENDER'] = "brianadrian81@gmail.com"
 
 mail = Mail(app)
 
